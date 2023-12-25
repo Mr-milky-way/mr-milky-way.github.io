894483eb4f068ce6849df3a78f7d1f269a73bc0d
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2023-12-24 16:23:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const chars = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
 const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
       randomString = length => Array.from(Array(length)).map(randomChar.join("");
+const card = document.querySelector(".card"),
+      letters = card.querySelector(".card-letters");
+const handleOnMove = e => {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+      letters.style.setProperty("--x", `${x}px`);
+      letters.style.setProperty("--y", `${y}px`);
+      
+      letters.innerText = randomString(1500);
+}
+
+card.onmousemove = e => handleOnMove(e);
+
+card.ontouchmove = e => handleOnMove(e.touches[0]);
