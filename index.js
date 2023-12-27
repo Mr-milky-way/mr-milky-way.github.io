7fae9eda62b36f990e59c894c72bcec247003be2
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2023-12-27 09:00:46 -0800

    fixed the background

--- index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("menu");
-
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
