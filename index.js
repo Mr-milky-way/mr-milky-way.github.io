13248c23df582c501e2d9fca50818e158ce892fb
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2024-01-04 13:37:02 -0800

    added words

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function openMenu() {
 function closeMenu() {
   document.getElementById("menu").style.width = "0";
 }
+
+const subtitle = document.getElementsByClassName("card-subtitle")[0];
+
+const createWord = (text, index) => {
+  const word = document.createElement("span");
+  
+  word.innerHTML = `${text} `;
+  
+  word.classList.add("card-subtitle-word");
+  
+  word.style.transitionDelay = `${index * 40}ms`;
+  
+  return word;
+}
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+
+const createSubtitle = text => text.split(" ").map(addWord);
+
+createSubtitle("But in a much more real sense, it's the best one.");
